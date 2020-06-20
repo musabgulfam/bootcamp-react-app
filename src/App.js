2dccbf83383fb30202f16b892af50c13b35092bc
Musab Gulfam <musabgulfam0722@gmail.com>
2020-06-20 15:00:04 +0500

    Beginner react app
    Created react app with a counter and more

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
+import {Message} from './Message'
 
 function App() {
+  let [count, setCount]=useState(1);
+  let [isDay, setDay]=useState(false);
   return (
-    <div className="App">
-     <h1>Hello world from all of us, from Earth!</h1>
+    <div className={`${isDay ? 'dayLight' : 'box'}`}>
+
+      <h1>It's {isDay ? "Morning" : "Night"}</h1>
+      
+      <Message counter={count} />
+      
+      <br />
+      
+      <button onClick={()=>{
+        setDay(!isDay)
+      }}>Shift</button>
+      
+      <button onClick={() => setCount(count + 1)}>
+        Update Counter
+      </button>
+    
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Message(props){
+  return <h1>Value of counter variable is {props.counter}</h1>
+}
