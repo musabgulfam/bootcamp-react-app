f4cda7725070a7f629c09f5b05dc870ba35632ce
musabgulfam <40719485+musabgulfam@users.noreply.github.com>
2020-06-21 11:49:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
+import Parent from './Parent';
+import CounterContext from './CounterContext';
 
 function App() {
+  let countState = useState(1) //[count, setCount]
+
   return (
-    <div className="App">
-     <h1>Hello world from all of us, from Earth!</h1>
-    </div>
+    <CounterContext.Provider value={countState}>
+      <div>
+        <Parent />
+      </div>
+    </CounterContext.Provider>
   );
 }
 
